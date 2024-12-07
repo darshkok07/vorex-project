7d61514d97cad755da137f1e75a32757091c10b6
FIRST_NAME LAST_NAME <charantm8787@gmail.com>
2024-12-07 23:25:49 +0530

    added new icons and design to base.html

--- base/views.py
+++ base/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 def home(request):
-    return HttpResponse('home page')
+    return render(request, 'base/home.html')
 
 def rooms(request):
-    return HttpResponse('Rooms')
+    return render(request, 'rooms.html')

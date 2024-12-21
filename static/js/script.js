885c3e73e482449608f0a9bae7e7321ebe8c17d2
FIRST_NAME LAST_NAME <charantm8787@gmail.com>
2024-12-22 02:49:55 +0530

    adding third psrty authentication

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,13 @@ const menuBtn = document.getElementById("menuBtn");
 menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
 });
+
+// window.addEventListener("load", () => {
+//   setTimeout(() => {
+//     const preloader = document.getElementById("preloader");
+//     preloader.style.opacity = "0";
+//     setTimeout(() => {
+//       preloader.style.display = "none";
+//     }, 100);
+//   }, 500);
+// });
